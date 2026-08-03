ed1644bdd675eb1db6b32bd02c78509f316cad6f
udaybhardwaj09 <udaybhardwaj.2006@gmail.com>
2026-08-03 14:38:45 +0530

    Test commit

--- index.js
+++ index.js
@@ -2,3 +2,12 @@ function sum(a,b){
     return a+b;
 }
 console.log(sum(5,10));
+
+function login(msg,error){
+    if(error){
+        console.log(error);
+    }
+    else{
+        console.log(msg);
+    }
+}
