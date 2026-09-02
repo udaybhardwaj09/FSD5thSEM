347344d86479e394c66d701ed15145ecc9d871cf
udaybhardwaj09 <udaybhardwaj.2006@gmail.com>
2026-09-02 09:00:52 +0530

    .

--- index.js
+++ index.js
@@ -1,70 +1,81 @@
-function sum(a,b){
-    return a+b;
+// function sum(a,b){
+//     return a+b;
+// }
+// console.log(sum(5,10));
+
+// function login(msg,error){
+//     if(error){
+//         console.log(error);
+//     }
+//     else{
+//         console.log(msg);
+//     }
+// }
+
+// function loginHandler(username,password,clbk){
+//     if(username == "ptomer40" && password == "12345"){
+//         clbk("success");
+//     }
+//     else{
+//         clbk("username or password is incorrect");
+//     }
+// }
+
+// function loginWithPromise(username, password) {
+//     return new Promise((resolve, reject) => {
+//         if (username === "ptomer40" && password === "12345") {
+//             resolve("Login successful");
+//         } else {
+//             reject("Username or password is incorrect");
+//         }
+//     });
+// }
+
+// loginWithPromise("ptomer40", "12345")
+//     .then((message) => console.log(message))
+//     .catch((error) => console.log(error));
+
+// loginWithPromise("wrongUser", "wrongPassword")
+//     .then((message) => console.log(message))
+//     .catch((error) => console.log(error));
+
+
+// async function handleLogin() {
+//     const status = await myPromise;
+//     if(status == "success"){
+//         console.log("Hi inside success");
+//         const orderStatus = await orderRecieved();
+//         console.log(orderStatus);
+//     }
+// }
+
+
+// function verifyOtp(otp) {
+//     return new Promise((resolve, reject) => {
+//         if (otp === "1234") {
+//             resolve("OTP verified");
+//         } else {
+//             reject("Invalid OTP");
+//         }
+//     });
+// }
+
+// function completeOrder(otp) {
+//     verifyOtp(otp)
+//         .then((message) => console.log(message + " - Order completed"))
+//         .catch((error) => console.log(error + " - Order not completed"));
+// }
+
+// completeOrder("1234");
+// completeOrder("0000");
+
+
+const root = document.getElementById("container");
+console.log(root);
+
+async function getData(){
+    const serverData = await fetch();
+    const jsonData = await serverData.json();
+    console.log(jsonData[0].title);
 }
-console.log(sum(5,10));
-
-function login(msg,error){
-    if(error){
-        console.log(error);
-    }
-    else{
-        console.log(msg);
-    }
-}
-
-function loginHandler(username,password,clbk){
-    if(username == "ptomer40" && password == "12345"){
-        clbk("success");
-    }
-    else{
-        clbk("username or password is incorrect");
-    }
-}
-
-function loginWithPromise(username, password) {
-    return new Promise((resolve, reject) => {
-        if (username === "ptomer40" && password === "12345") {
-            resolve("Login successful");
-        } else {
-            reject("Username or password is incorrect");
-        }
-    });
-}
-
-loginWithPromise("ptomer40", "12345")
-    .then((message) => console.log(message))
-    .catch((error) => console.log(error));
-
-loginWithPromise("wrongUser", "wrongPassword")
-    .then((message) => console.log(message))
-    .catch((error) => console.log(error));
-
-
-async function handleLogin() {
-    const status = await myPromise;
-    if(status == "success"){
-        console.log("Hi inside success");
-        const orderStatus = await orderRecieved();
-        console.log(orderStatus);
-    }
-}
-
-
-function verifyOtp(otp) {
-    return new Promise((resolve, reject) => {
-        if (otp === "1234") {
-            resolve("OTP verified");
-        } else {
-            reject("Invalid OTP");
-        }
-    });
-}
-
-function completeOrder(otp) {
-    verifyOtp(otp)
-        .then((message) => console.log(message + " - Order completed"))
-        .catch((error) => console.log(error + " - Order not completed"));
-}
-
-completeOrder("1234");
-completeOrder("0000");
+button.addEventListeners("click",getData);
